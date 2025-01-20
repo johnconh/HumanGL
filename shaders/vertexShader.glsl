0138a08fbb7d34325678829d1c0ba5ad81c5b5b9
johnconh <sevenballs@gmail.com>
2025-01-20 20:37:46 +0100

    vertex para animator.

--- shaders/vertexShader.glsl
+++ shaders/vertexShader.glsl
@@ -1,7 +1,12 @@
 #version 330 core
+
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
+layout (location = 3) in vec3 aTangent;
+layout (location = 4) in vec3 aBitangent;
+layout (location = 5) in ivec4 aBoneIds;
+layout (location = 6) in vec4 aWeigths;
 
 out vec2 TexCoords;
 
@@ -9,8 +14,26 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+const int MAX_BONES = 100;
+const int MAX_BONE_INFLUENCE = 4;
+uniform mat4 finalBonesMatrices[MAX_BONES];
+
 void main()
 {
+    vec4 totalPosition = vec4(0.0f);
+    for(int i = 0; i < MAX_BONE_INFLUENCE; i++)
+    {
+        if(aBoneIds[i] == -1)
+            continue;
+        if(aBoneIds[i] >= MAX_BONES)
+        {
+            totalPosition = vec4(pos,1.0f);
+            break;
+        }
+        vec4 localPosition = finalBonesMatrices[aBoneIds[i]] * vec4(pos,1.0f);
+        totalPosition += localPosition * aWeigths[i];
+        vec3 localNormal = mat3 (finalBonesMatrices[aBoneIds[i]]) * aNormal;
+    }
     TexCoords = aTexCoords;    
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    gl_Position = projection * view * model * totalPosition;
 }
